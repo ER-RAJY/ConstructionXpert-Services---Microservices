e85645f9e71132d73c9249f9e646ef2797b207cf
ER-RAJY <noureddineerajy@gmail.com>
2024-09-06 16:48:05 +0100

    update class user name to Utilisateur

---
+++ ConstructionXpert(Partie 2)/Gestion_utilisateurs/src/main/java/com/utilisateurs/Model/Utilisateur.java
@@ -0,0 +1,7 @@
+package com.utilisateurs.Model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Utilisateur extends  Personne{
+}
--- ConstructionXpert(Partie 2)/Gestion_utilisateurs/src/main/resources/db/migration/V1__create_table_Personne.sql
+++ ConstructionXpert(Partie 2)/Gestion_utilisateurs/src/main/resources/db/migration/V1__create_table_Personne.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Personne (
-                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                          id BIGSERIAL PRIMARY KEY,
                           username VARCHAR(255) NOT NULL,
                           email VARCHAR(200) NOT NULL,
                           password VARCHAR(200) NOT NULL
