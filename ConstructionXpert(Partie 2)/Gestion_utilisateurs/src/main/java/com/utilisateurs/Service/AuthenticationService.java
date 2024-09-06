a8523bbf12151a05e8b166e80074d01730b0b3c8
ER-RAJY <noureddineerajy@gmail.com>
2024-09-06 16:47:19 +0100

    add methode authenticate user

--- ConstructionXpert(Partie 2)/Gestion_utilisateurs/src/main/java/com/utilisateurs/Service/AuthenticationService.java
+++ ConstructionXpert(Partie 2)/Gestion_utilisateurs/src/main/java/com/utilisateurs/Service/AuthenticationService.java
@@ -5,7 +5,7 @@ import com.utilisateurs.Dto.AuthenticationRequest;
 import com.utilisateurs.Dto.AuthenticationResponse;
 import com.utilisateurs.Dto.RegisterRequest;
 import com.utilisateurs.Model.Role;
-import com.utilisateurs.Model.User;
+import com.utilisateurs.Model.Utilisateur;
 import com.utilisateurs.Repository.PersonneRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -23,7 +23,7 @@ public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
-        var user = new User();
+        var user = new Utilisateur();
         user.setUsername(request.getUsername());
         user.setEmail(request.getEmail());
         user.setPassword(passwordEncoder.encode(request.getPassword()));
