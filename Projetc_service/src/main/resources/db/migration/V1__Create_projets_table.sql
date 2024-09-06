6b4cc146217baddeaf29927275753e8cc5e71bfa
ER-RAJY <noureddineerajy@gmail.com>
2024-09-06 09:57:39 +0100

    result fiinal

--- Projetc_service/src/main/resources/db/migration/V1__Create_projets_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE projets (
-                         id BIGSERIAL PRIMARY KEY,
-                         nom VARCHAR(255) NOT NULL,
-                         description TEXT,
-                         date_debut DATE,
-                         date_fin DATE,
-                         budget DOUBLE PRECISION
-);
